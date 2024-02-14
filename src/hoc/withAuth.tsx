16f9c64c1f5a4fd18339d4b9797fd31dc0afbd28
Kent Jordan <kentaki.dev@gmail.com>
2024-02-14 14:51:46 +0800

    Refactored withAuth

--- src/hoc/withAuth.tsx
+++ src/hoc/withAuth.tsx
@@ -4,22 +4,16 @@ import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { setAccessToken } from "@/redux/app.slice";
-import { useSelector } from "react-redux";
-import { RootState } from "@/redux/store";
 
 const withAuth = (Component: () => JSX.Element) => {
   return () => {
     const router = useRouter();
     const dispatch = useDispatch();
-    const { access_token } = useSelector((state: RootState) => state.appReducer);
 
     useEffect(() => {
-      // TODO: Refresh the token first
-      // If successfully refreshed, proceed to the auth'd routes
-
       const refreshTokens = async () => {
         try {
-          const res = await customAxios.get("auth/refresh", {
+          const res = await customAxios.get("auth/refresh/admin", {
             withCredentials: true,
           });
           dispatch(setAccessToken(res.data.access_token));
@@ -29,7 +23,7 @@ const withAuth = (Component: () => JSX.Element) => {
         }
       };
 
-      if (!access_token) refreshTokens();
+      refreshTokens();
     }, []);
 
     return <Component />;
