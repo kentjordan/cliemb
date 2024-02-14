a5018725e949bfaa8555d5d8de5cd40d6a67e604
Kent Jordan <kentaki.dev@gmail.com>
2024-02-14 15:00:14 +0800

    Added interface for UserData

---
+++ src/types/IUserData.ts
@@ -0,0 +1,11 @@
+export default interface IUserData {
+    id: string;
+    first_name: string;
+    last_name: string;
+    email: string;
+    emergency_no: string;
+    medical_conditions: string;
+    province: string;
+    city: string;
+    barangay: string;
+}
