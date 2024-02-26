e8ac30671122cb7eb2fb2f6ab0e046c7faa298fe
Kent Jordan <kentaki.dev@gmail.com>
2024-02-26 16:41:45 +0800

    Fixed root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
 "use client";
+
 import withAuth from "@/hoc/withAuth";
+import useAppState from "@/hooks/useAppState";
+import { useRouter } from "next/navigation";
+import { useEffect, useLayoutEffect } from "react";
 
 const Home = () => {
+  const router = useRouter();
+
+  const { access_token } = useAppState();
+
+  useEffect(() => {
+    if (access_token) router.replace("/dashboard");
+    if (!access_token) router.replace("/login");
+  }, [access_token]);
+
   return <div></div>;
 };
 
