f5a2cc8fede512359531b2badbb50b844a7ba8b8
Kent Jordan <kentaki.dev@gmail.com>
2024-03-06 18:57:16 +0800

    Temp added production hostnames

--- next.config.js
+++ next.config.js
@@ -7,8 +7,8 @@ const nextConfig = {
   env: {
     jwt_secret_key: `-7_z~&|ne<fw&3I3'7IsCb^#7}2hjo2{+^Zl3(Rv{9}[T:&5LJ`,
     protocol: process.env.NODE_ENV === "production" ? "https" : "http",
-    rest_hostname: process.env.NODE_ENV === "production" ? "" : "localhost:5000/api/",
-    ws_hostname: process.env.NODE_ENV === "production" ? "" : "localhost:5001/ws",
+    rest_hostname: process.env.NODE_ENV === "production" ? "89.116.134.135:5000/api/" : "localhost:5000/api/",
+    ws_hostname: process.env.NODE_ENV === "production" ? "89.116.134.135:5001/ws" : "localhost:5001/ws",
   },
 };
 
