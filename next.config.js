7604048411c02677eb420440f542ccf2d01ea40d
Kent Jordan <kentaki.dev@gmail.com>
2024-03-07 12:58:08 +0000

    Added domain in env, and sharp

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [{ hostname: "localhost" }, { hostname: "89.116.134.135" }],
+    remotePatterns: [{ hostname: "localhost" }, { hostname: "cliemb.online" }],
   },
   reactStrictMode: false,
   env: {
     jwt_secret_key: `-7_z~&|ne<fw&3I3'7IsCb^#7}2hjo2{+^Zl3(Rv{9}[T:&5LJ`,
     protocol: process.env.NODE_ENV === "production" ? "https" : "http",
-    rest_hostname: process.env.NODE_ENV === "production" ? "89.116.134.135:5000/api/" : "localhost:5000/api/",
-    ws_hostname: process.env.NODE_ENV === "production" ? "89.116.134.135:5001/ws" : "localhost:5001/ws",
+    rest_hostname: process.env.NODE_ENV === "production" ? "cliemb.online/api/" : "localhost:5000/api/",
+    ws_hostname: process.env.NODE_ENV === "production" ? "cliemb.online/ws" : "localhost:5001/ws",
   },
 };
 
