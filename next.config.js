e6d89511ccf7933c52b46a132a8cc21ec52f5a5c
Kent Jorda <kentaki.dev@gmail.com>
2024-03-07 21:19:16 +0800

    Refactored socket.io

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@ const nextConfig = {
   env: {
     jwt_secret_key: `-7_z~&|ne<fw&3I3'7IsCb^#7}2hjo2{+^Zl3(Rv{9}[T:&5LJ`,
     protocol: process.env.NODE_ENV === "production" ? "https" : "http",
+    hostname: process.env.NODE_ENV === "production" ? "cliemb.online" : "localhost:5000",
     rest_hostname: process.env.NODE_ENV === "production" ? "cliemb.online/api/" : "localhost:5000/api/",
     ws_hostname: process.env.NODE_ENV === "production" ? "cliemb.online/ws" : "localhost:5001/ws",
   },
