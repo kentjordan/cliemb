1d4cc287fb177128f779165afb317cebd01fa1f7
Kent Jordan <kentaki.dev@gmail.com>
2024-04-12 11:30:52 +0800

    Added: icons on dashboard selections

--- src/app/(authd)/dashboard/page.tsx
+++ src/app/(authd)/dashboard/page.tsx
@@ -1,23 +1,26 @@
 "use client";
 import withAuth from "@/hoc/withAuth";
-import { useSelector } from "react-redux";
-import { RootState } from "@/redux/store";
 import Link from "next/link";
+import ic_cit from "@/assets/ic_cit.png";
+import ic_emergency_hotlines from "@/assets/ic_emergency_hotlines.png";
+import ic_monitoring_system from "@/assets/ic_monitoring_system.png";
+import Image from "next/image";
 
 const DashboardPage = () => {
-  const { access_token } = useSelector((state: RootState) => state.appReducer);
-
   return (
     <div className="flex w-full items-center justify-center bg-white p-4">
-      <div className="flex flex-col gap-4">
-        <Link className="bg-red-700 p-8 text-2xl text-white" href="/dashboard/cit-department">
-          CIT Department
+      <div className="flex flex-col gap-y-10">
+        <Link className="flex items-center" href="/dashboard/cit-department">
+          <Image src={ic_cit} alt="" width={150} height={150} />
+          <p className="w-80 bg-red-700 p-8 text-2xl text-white">CIT Department</p>
         </Link>
-        <Link className="bg-red-700 p-8 text-2xl text-white" href="/dashboard/emergency-hotlines">
-          Emergency Hotlines
+        <Link className="flex items-center" href="/dashboard/emergency-hotlines">
+          <Image src={ic_emergency_hotlines} alt="" width={150} height={150} />
+          <p className="w-80 bg-red-700 p-8 text-2xl text-white">Emergency Hotlines</p>
         </Link>
-        <Link className="bg-red-700 p-8 text-2xl text-white" href="/dashboard/monitoring-system">
-          Monitoring System
+        <Link className="flex items-center" href="/dashboard/monitoring-system">
+          <Image src={ic_monitoring_system} alt="" width={150} height={150} />
+          <p className="w-80 bg-red-700 p-8 text-2xl text-white">Monitoring System</p>
         </Link>
       </div>
     </div>
